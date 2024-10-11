f2381a6e47a52eb3cc5d350b9188f7f61543c6f5
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-10-11 13:19:21 +0000

    atualizar

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pyhton-bd2
+
+## objetivo :
+criar CRUD com python em uma estrutura de diretorios
+
+### tecnologias:
+- ORM : SQlALchemy
+- Docker: Banco de dados MySQL
+
+
---
+++ app/config/connection.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+
+# paramentros para conexão com banco de dados MySQL
+
+db_user= "user"
+db_password="user_password"
+db_host = "localhost"
+db_port ="3306"
+db_name= "meu banco"
+
+# URL da conexão  para o banco de dados MySQL
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+# Conectando ao banco de dados
+
+db= create_engine(DATABASE_URL)
+Session = sessionmaker(bind=db)
+session = Session()
+
+@contextmanager
+def get_db():
+    db= Session
+    try:
+        yield db
+        db.commit()
+    except Exception as e:
+        raise e 
+    finally:
+        db.close()
+
